f266c34988a957d79e2fced0c3a6a633c86d6858
mr.kip@ntlworld.com <chris@predator.(none)>
2011-09-11 13:24:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rebus::Application.config.secret_token = 'ab3df010ee36a13812ea7261e54d986af51ce33e5fe3c5fa79532f5cf27a5eee1c13c46b597b123df4b84211e2db4d264c5b14ff7d2590cb72a35b1bd208b480'
